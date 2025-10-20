c76c3598cdf7bfe4473c94ccc4f6fa8e69d40774
DiegoG0121 <dgarciamain20.us@gmail.com>
2025-10-20 09:45:51 +0000

    updated

---
+++ ps02_starter/calc.c
@@ -0,0 +1,86 @@
+/* Implementing a basic arithmetic calculator */
+
+#include <stdio.h>
+
+int add(int num1, int num2){
+    return num1+num2;
+}
+
+int subtract(int num1, int num2){
+    return num1-num2;
+}
+
+int multiply(int num1, int num2){
+    return num1*num2;
+}
+
+double divide(int num1, int num2){
+    return num1/num2;
+}
+
+int modulus(int num1, int num2){
+    return num1%num2;
+}
+
+int main() {
+    int choice, num1, num2;
+    float result;
+
+    /* TODO: display the menu of options and take user input, perform the chosen operation and display result.
+             your program should continue until the user chooses to exit. 
+             format your print statements exactly as the ones shown in sample output in the pdf */
+
+    start_loop:
+    while(1){
+        printf("Press 1 for addition (+)\nPress 2 for subtraction (-)\nPress 3 for multiplication (*)\nPress 4 for division (/)\nPress 5 for modulus (%)\nPress 6 for exit\n");
+
+        printf("Please enter your choice (1-6): ");
+        scanf("%d", &choice);
+
+        if(choice == 6){
+            break;
+        }
+
+        printf("Please enter two integers: ");
+        scanf("%d %d", &num1, &num2);
+
+        if(num1 == 0 || num2 == 0){
+            if(choice == 4){
+                printf("Error: Division by zero is not allowed.\n");
+                goto start_loop;
+            } else {
+                printf("Error: Modulus by zero is not allowed.\n");
+                goto start_loop;
+            }
+        }
+
+        switch(choice){
+            case 1:
+                printf("Result: %d + %d = %.2lf\n", num1, num2, (double)add(num1,num2));
+                break;
+            case 2:
+                printf("Result: %d - %d = %.2lf\n", num1, num2, (double)subtract(num1,num2));
+                break;
+            case 3:
+                printf("Result: %d * %d = %.2lf\n", num1, num2, (double)multiply(num1,num2));
+                break;
+            case 4:
+                printf("Result: %d / %d = %.2lf\n", num1, num2, divide(num1,num2));
+                break;
+            case 5:
+                printf("Result: %d % %d = %.2lf\n", num1, num2, (double)modulus(num1,num2));
+                break;
+            
+            default:
+                printf("Invalid choice. Please enter a number from 1 to 6.\n");
+                break;
+        }
+
+    }
+
+    printf("Exiting the program. Thank you!\n");
+    
+
+
+    return 0;
+}
---
+++ ps02_starter/decr_tri.c
@@ -0,0 +1,30 @@
+/* Implement a decreasing triangle of asterisks. An example of the triangle consisting of n=5 rows is shown below. 
+
+* * * * *
+* * * *
+* * *
+* *
+*
+
+*/
+
+#include<stdio.h>
+
+int main() {
+    int n;
+    printf("Enter n (number of rows): ");
+
+    /* TODO: read the value for n and print the first n rows of the above pattern */
+
+    scanf("%d", &n);
+
+    for(int x = 0; x < n-1; x++ ){
+        for(int c = n; c > 0; c-- ){
+            printf("*");
+        }
+        printf("\n");
+    }
+
+
+    return 0;
+}

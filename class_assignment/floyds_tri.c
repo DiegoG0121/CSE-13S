de15a9a43f6dbf8acee41a824078dfd3e5e69c94
DiegoG0121 <dgarciamain20.us@gmail.com>
2025-10-20 07:58:29 +0000

    adding files and placing them in thr right folder

---
+++ class_assignment/floyds_tri.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+
+void myName(name){
+
+    if(if){
+
+    }
+}
+
+int main(){
+    int userIn;
+    printf("Enter 3 digit #: ");
+    scanf("%d", &userIn);
+
+    int x = 0;
+    while(x != userIn){
+        for(int x = 1; x <= userIn; x++){
+            for(int ){
+
+            }
+        }
+    }
+}
